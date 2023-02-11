b164271f5255094fbdf166376d4046be73b17852
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-02-11 11:54:08 +0530

    perlogo loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./App.css";
 
-import {BrowserRouter,  Routes, Route } from "react-router-dom";
+import {BrowserRouter,  Routes, Route ,useLocation} from "react-router-dom";
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -9,13 +9,18 @@ import Contact from './pages/Contact'
 
 
 function App(){
+  const location= useLocation();
+  window.scrollTo(0, 0);
+
   return (<>
     <BrowserRouter>
-    <Routes>
+    <Routes location ={location} key={location.pathname}>
+
        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
+
        </Route>
     </Routes>
     </BrowserRouter>
